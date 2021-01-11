59b289013794de6dcbfaaebf08da946ca76ef5e7
Simone Zanin <zabi94@users.noreply.github.com>
2021-01-11 18:58:29 +0100

    Removed unused import

--- src/main/java/simone_zanin/imagetagger/imagetaggerserver/Heartbeat.java
+++ src/main/java/simone_zanin/imagetagger/imagetaggerserver/Heartbeat.java
@@ -6,7 +6,6 @@
 package simone_zanin.imagetagger.imagetaggerserver;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
